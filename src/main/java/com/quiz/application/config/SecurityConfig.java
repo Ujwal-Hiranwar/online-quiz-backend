c43a12af406389e9bac8d636d338b28f73ee34fb
Ujwal-Hiranwar <ujjwalhiranwar15@gmail.com>
2025-10-19 09:44:19 +0000

    pushed updated changes

--- src/main/java/com/quiz/application/config/SecurityConfig.java
+++ src/main/java/com/quiz/application/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://online-quizz-frontend.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
